b202482a03ec64f37a6246eb975fda65ac95cffd
Ryan <kuhnrl30@gmail.com>
2017-02-26 13:51:02 -0500

    fix travis issues

---
+++ R/NYTimes.R
@@ -0,0 +1,17 @@
+#' Training dataset
+#'
+#' This dataset was downloaded from Kaggle
+
+#' @name trainingset
+#' @docType data
+#' @format dataframe
+NULL
+
+#' Test dataset
+#'
+#' This dataset was downloaded from Kaggle
+
+#' @name testingset
+#' @docType data
+#' @format dataframe
+NULL
